92d817c64e0baa262ebcd615bee3f324b7445361
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2023-09-10 17:05:09 +0200

    cleanup

--- src/assets/form/form.tsx
+++ src/assets/form/form.tsx
@@ -1,37 +1,17 @@
 import { handleForm } from "./handleForm";
 
 export default function Form() {
-  /*function handleForm(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-
-    const form = e.currentTarget;
-
-    interface formData {
-      username: string;
-      password: string;
-    }
-
-    const formData = new FormData(form);
-    /* {
-         username: form.username.value,
-         password: form.password.value,
-       };
-
-    const formJson = Object.fromEntries(formData.entries());
-    console.log(formJson);
-  }*/
-
   return (
     <div className="container">
       <form action="post" onSubmit={handleForm}>
         <h1>Create New Account:</h1>
         <div className="field">
           <label htmlFor="username">username: </label>
-          <input type="text" name="username" id="username" /*placeholder="username"*/ />
+          <input type="text" name="username" id="username" />
         </div>
         <div className="field">
           <label htmlFor="password">password: </label>
-          <input type="password" name="password" id="password" /*placeholder="username"*/ />
+          <input type="password" name="password" id="password" />
         </div>
         <input type="submit" value="Create" />
       </form>
