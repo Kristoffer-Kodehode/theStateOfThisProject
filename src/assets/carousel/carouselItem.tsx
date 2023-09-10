5dc9fcb441fe8fa52b7a2e67fea10eb8b44fb625
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2023-09-10 16:00:09 +0200

    another attempt to fix type

--- src/assets/carousel/carouselItem.tsx
+++ src/assets/carousel/carouselItem.tsx
@@ -1,13 +1,9 @@
-export default function CarouselItem(properties: {
-  imgURL: string;
-  title: string;
-  imgId: string;
-}) {
-  const { imgURL, title, imgId } = properties;
+export default function CarouselItem(properties: { imgURL: string; title: string; id: string }) {
+  const { imgURL, title, id } = properties;
 
   return (
     <>
-      <img className="galleryImg" id={imgId} src={imgURL} alt={title} />
+      <img className="galleryImg" id={id} src={imgURL} alt={title} />
     </>
   );
 }
